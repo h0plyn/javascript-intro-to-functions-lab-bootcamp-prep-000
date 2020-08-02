55efbd7bc00d2a48b09bcb7a5a94b5808c02375c
Ricky Rhodes <ricky@rickyrhodes.com>
2020-08-02 23:57:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function logWhisper(string) {
 };
 
 function sayHiToGrandma(string) {
-  string === string.toLowerCase() ? "I can't hear you";
-  string === string.toUpperCase() ? "YES INDEED!";
-  string === "I love you, Grandma" ? "I love you, too";
-};
+  if (string === string.toLowerCase()) { 
+    return "I can't hear you"
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else if (string === "I love you, Grandma") {
+      return "I love you, too";
+  };
