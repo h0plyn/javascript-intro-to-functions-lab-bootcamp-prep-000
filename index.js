78d1478908326ddc27611b0b9c43cfde868f4cf2
Ricky Rhodes <ricky@rickyrhodes.com>
2020-08-02 23:55:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function shout(string) {
   return string.toUpperCase();
 };
+
+function whisper(string) {
+  return string.toLowerCase();
+};
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+};
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+};
+
+function sayHiToGrandma(string) {
+  string === string.toLowerCase() ? "I can't hear you"
+  string === string.toUpperCase() ? "YES INDEED!"
+  string === "I love you, Grandma" ? "I love you, too"
+};
